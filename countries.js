0ec0b052b4ce9eeeb6bc67825270d459cbf5bcac
olikelly00 <oliverkelly1995@hotmail.co.uk>
2024-08-02 11:03:13 +0100

    added some helper functions for countries table

---
+++ countries.js
@@ -0,0 +1,56 @@
+
+// Import the 'pool' object so our helper functions can interact with the PostgreSQL database
+import { pool } from "./db/index.js";
+
+
+export async function getCountries() {
+
+  const queryText = "SELECT * FROM countries";
+
+  const result = await pool.query(queryText);
+
+  return result.rows;
+}
+
+console.log(getCountries())
+
+
+
+export async function getCountryByID(id) {
+  
+const queryText = "SELECT * FROM countries WHERE id = $1";
+
+const result = await pool.query(queryText, [id]);
+
+return result.rows[0] || null;
+}
+
+export async function createCountry(country) {
+  
+  const newCountry = {
+    ...country
+  }
+  const queryText = "INSERT INTO countries (country_name, speaks_english) VALUES ($1, $2)";
+
+  const result = await pool.query(queryText, [newCountry.name, newCountry.speaks_english]);
+
+ 
+  return result || null;
+}
+
+
+export async function updateCountryById(id, updates) {
+    const updatedCountry = {
+      ...updates
+    };
+    const queryText = "UPDATE countries SET country_name = $1, speaks_english = $2";
+  
+    const result = await pool.query(queryText, [updatedCountry.country_name, updatedCountry.speaks_english, id]);
+  
+    return result || null;
+  }
+//updateCountryById(1, {"country_name": "Germany", "speaks_english": "false"})
+
+export async function deleteCountryById(id) {
+  // Query the database to delete the resource and return the deleted resource or null
+}
